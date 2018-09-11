d1e9cad17f21dca9d8dc57488b7623f18e4a98cc
tm2018 <31844089+tm2018@users.noreply.github.com>
2018-09-11 14:29:19 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd `dirname $0`
+cd /home/shadowsocksr
 python_ver=$(ls /usr/bin|grep -e "^python[23]\.[1-9]\+$"|tail -1)
 eval $(ps -ef | grep "[0-9] ${python_ver} server\\.py m" | awk '{print "kill "$2}')
 ulimit -n 512000
